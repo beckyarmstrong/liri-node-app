e7ea4956ea68f32bf66ae17f16b9872b4229a66e
beckyarmstrong <becky.armstrong14@gmail.com>
2018-02-14 13:09:58 -0700

    Final Commit

---
+++ keys.js
@@ -0,0 +1,11 @@
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'NuKC6Pf3aEUxyKEFLn3NP9Deg',
+  consumer_secret: 'NXDRusuyPodfQEuyMPzXF51J3SiIgrY6qBqBN4uTS2QQj4gWEm',
+  access_token_key: '963848976171749376-GcSkcuFrldhS1z1GyDsULc34Zf6mxFw',
+  access_token_secret: 'ho55xoF5xvnndrvmhBDGJXsEVcYDrS1fyNPd9Udv3OJlC',
+}
+
+module.exports = twitterKeys;
